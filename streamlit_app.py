14030ea16ad7ef80b94bf67914998b0a69330fe2
lilyyoung02 <ly274@cornell.edu>
2025-03-14 14:31:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("Streamlit")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("User Input")
+st.headers
+x = st.text_input("Input 1: ")
+st.write(f"The first input is: {x}")
+#st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+is_clicked = st.button("Click Me")
+st.write('##this is a title: ')
