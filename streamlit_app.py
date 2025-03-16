8c64e3cb5ecd66550121e582967d77307f82c73d
lilyyoung02 <ly274@cornell.edu>
2025-03-16 14:30:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
-st.title("User Input")
-st.headers
-x = st.text_input("Input 1: ")
-st.write(f"The first input is: {x}")
+st.set_page_config(page_title="Blood Logistics Tool", layout="wide")
+
+st.title("ONR Blood Mangement Support Tool")
+st.sidebar.header("User Input")
+
+#st.title("User Input")
+#st.headers
+#x = st.text_input("Input 1: ")
+#st.write(f"The first input is: {x}")
 #st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
-is_clicked = st.button("Click Me")
-st.write('##this is a title: ')
+#is_clicked = st.button("Click Me")
+#st.write('##this is a title: ')
